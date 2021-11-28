d32442ce5bd6a073507f05a23dc592f6c0b877cc
ANIL KUMAR PASWAN <93995564+anilkumarpaswan@users.noreply.github.com>
2021-11-29 00:46:17 +0530

    java Program Leapyear

---
+++ LeapYear.java
@@ -0,0 +1,19 @@
+class LeapYear{
+     public static void main(String agrs[]){
+	 int year=400;
+	 if(year%400==0){
+	 System.out.println("That is LeapYear");
+	 }
+	 if(year%4==0){
+		 System.out.println("That is LeapYear2");
+		 
+	 }
+	 if(year%100!=0){
+		 
+	 }
+	 
+	 else{
+		 System.out.println("That is not LeapYear");
+	 }
+	 }
+}
